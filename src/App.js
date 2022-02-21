a9a4239c5d0154b2d5632b144bd67df583cee065
killerkc12 <kiranchavankc12@gmail.com>
2022-02-21 21:00:35 +0530

    development msg added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is new branch call as development
+          This is new branch call as development  
         </p>
         <a
           className="App-link"
